af9f547f39d02b281854855935ee5aa5e280deb9
mrbaker1917 <mrbaker1917@gmail.com>
2026-03-17 15:29:28 -0700

    updates to cdn for video links

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -119,7 +119,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+	videoURL := fmt.Sprintf("%s/%s", cfg.s3CfDistribution, key)
 	video.VideoURL = &videoURL
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
